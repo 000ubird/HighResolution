f5dbbc9563f3a0d895930e616f09f76898b8a182
000ubird <000ubird@gmail.com>
2015-08-27 19:34:28 +0900

    テストライブラリ内にテスト用メソッドを追加

--- libs/test/test.py
+++ libs/test/test.py
@@ -1,5 +1,8 @@
 def test(text):
     print(text)
 
+def test2(text):
+    print(text+"2!!!")
+
 if __name__ == '__main__':
     test()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
 from libs.test.test import test
+from libs.test.test import test2
 
 test("test")
+test2("test")
