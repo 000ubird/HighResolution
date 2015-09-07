1a1cf37f7dc2b110d7b01d57eb3633ff79c5b5f7
000ubird <000ubird@gmail.com>
2015-09-08 00:03:14 +0900

    mainメソッドに試験的な音声データの合成を実装

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from libs.fft.fft import fft
+from libs.audio.create import createSineWave2
+from libs.audio.wavio import writewav24
 
-fileName = "../wav/sample.wav"
+fileName = "../wav/sinwave.wav"
 
 fft(fileName)
+
+#sin波の書き出しと合成のテスト
+createSineWave2(96000, 3, 440, "3.wav")
+a = createSineWave2(44100, 2, 1000, "1000.wav")
+b = createSineWave2(44100, 2, 440, "440.wav")
+writewav24("test.wav",44100,a+b)
