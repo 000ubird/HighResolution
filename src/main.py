4123436903050b05df1d1fe0f17d53749ad1eab4
000ubird <000ubird@gmail.com>
2015-12-07 23:23:57 +0900

    振幅値を算出するメソッドを定義

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ from libs.audio.create import createSineWave2
 from libs.audio.wavio import writewav24
 from libs.audio.read import read_wav_cd
 
-fileName = "../wav/sample.wav"
+fileName = "../wav/02-Are You Real.wav"
+csvFileName = "../result_hi.csv"
+
+mixFlame = 100000
+maxFlame = 900000
 
 def getTest(wavData):
     array = []
@@ -16,6 +20,21 @@ def getTest(wavData):
         array.append(div)
         
     return array
+
+def getDivAndAmps(wavData):
+    divs = []
+    amps = []
+    
+    #for i in range(1,len(wavData)-1) : 
+    for i in range(mixFlame,maxFlame) : 
+        data1 = wavData[i-1]
+        data2 = wavData[i+1]
+        div = data2 - data1 #変化量の計算
+        
+        divs.append(div)    #現在の振幅値の前後の変化量
+        amps.append(wavData[i])      #現在の振幅値
+        
+    return {"divs" : divs, "amps" : amps}
 
 #CSVファイルに配列の要素を書き込み
 def writeText(wavData,csvName):
