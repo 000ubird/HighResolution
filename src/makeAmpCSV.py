35bf7b730ec899561d371e9bd6868d0854b7ffc4
000ubird <000ubird@gmail.com>
2016-01-08 01:00:11 +0900

    CD音源の振幅値のCSVファイルを出力する処理を追加

--- src/makeAmpCSV.py
+++ src/makeAmpCSV.py
@@ -2,16 +2,17 @@ import numpy as np
 import audioread as ar
 
 #ファイル名
-wavName = "../wav/sample_96000_24bit_001.wav"
-csvName = "../result_hi.csv"
-
-#サンプル数と開始フレーム
-sampleNum = 50000
-beginFlame = 100
-endFlame = beginFlame + sampleNum
+wavName_hi = "../wav/sample_hi.wav"
+wavName_cd = "../wav/sample_cd.wav"
+csvName_hi = "../result_hi.csv"
+csvName_cd = "../result_cd.csv"
 
 #取得する振幅値の数
 N = 20
+
+#抽出するフレーム数
+beginFlame = 0
+endFlame = 50000
 
 #参考: http://wrist.hatenablog.com/entry/2013/08/06/015240
 def pcm2float(short_ndary):
@@ -47,13 +48,13 @@ def read_wav_cd(wavName) :
     return {"amp_l":wav_float_l, "amp_r":wav_float_r}
 
 #指定したフレーム数分だけ振幅値をCSVで出力する
-def makeAmpCSV(wavData,csvName):
+def makeAmpCSV(wavData,csvName,begin,end):
     amps = [[]] #2次元配列の動的確保
     result = ""
     
     #指定したフレーム部分内の振幅値を取得
-    i = beginFlame
-    while i < endFlame :
+    i = begin
+    while i < end :
         #numAmp分だけ振幅値を抽出
         for j in range(i,i+N) : 
             amps.append(wavData[j])
@@ -72,16 +73,46 @@ def makeAmpCSV(wavData,csvName):
         f = open(csvName,'w')
         f.write(result)
         f.close()
-        print("CSVファイルを出力しました。")
     except : 
         print("CSVファイルの出力中にエラーが発生しました。")
         exit()
     
     return amps
 
+#指定したフレーム数分だけ振幅値をCSVで出力する - CD用
+def makeAmpCSV2(wavData,csvName,begin,end):
+    result = ""
+    
+    #指定したフレーム部分内の振幅値を取得
+    i = begin
+    while i < end :
+        #numAmp分だけ振幅値を抽出
+        for j in range(i,i+N,2) : 
+                result += repr(wavData[j])+','
+                result += repr(wavData[j])
+                #最後の列にはカンマを付けない
+                if j != i+N-2 : 
+                    result += ','
+                
+        result += '\n'
+        i += N
+        
+    #CSVファイルに格納
+    try : 
+        f = open(csvName,'w')
+        f.write(result)
+        f.close()
+    except : 
+        print("CSVファイルの出力中にエラーが発生しました。")
+        exit()
+
 if __name__ == '__main__':
     #WAVデータの読み込み
-    wav_data = read_wav_cd(wavName)
+    wav_data_hi = read_wav_cd(wavName_hi)
+    #wav_data_cd = read_wav_cd(wavName_cd)
     
     #CSVファイルの出力
-    makeAmpCSV(wav_data['amp_l'], csvName)
+    makeAmpCSV(wav_data_hi['amp_l'], csvName_hi,beginFlame,endFlame)
+    #現時点ではハイレゾ音源から擬似CD音源の特徴ベクトルを作成する
+    makeAmpCSV2(wav_data_hi['amp_l'], csvName_cd,beginFlame,endFlame)
+    print("CSVファイルを出力しました。")
