e4a9e5926b8617afc29796ce5de79a3c340b1ac2
000ubird <000ubird@gmail.com>
2016-01-09 23:19:21 +0900

    信号の変換と音源の生成処理を定義

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ from makeAmpCSV import makeAmpArrayCD
 from makeAmpCSV import read_wav_cd
 from makeGMM import makeGMM,debug_gmm
 from convert import convert
+from sklearn.externals import joblib
 
+import math
+import sys, time
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
+import wavio
 
 #学習に使う音声ファイル名
 WAV_NAME_GMM = "../wav/sample_96000_24bit_001.wav"
@@ -13,10 +17,11 @@ WAV_NAME_GMM = "../wav/sample_96000_24bit_001.wav"
 #抽出するフレーム数
 BEGIN_FLAME = 0  #固定
 END_FLAME = 380000
-
 #GMMの混合数
-components = 50
+components = 30
 
+'''
+#学習処理
 print("学習に使用する音声ファイルを読み込んでいます。")
 wav_data_hi = read_wav_cd(WAV_NAME_GMM,BEGIN_FLAME,END_FLAME)
 print("音声ファイルを読み込みました。\n")
@@ -32,24 +37,55 @@ print("学習データが生成されました。サンプル数は",int(END_FLA
 
 print("学習を実行します。")
 gmm = makeGMM(components,sample)
-debug_gmm(gmm)
+#debug_gmm(gmm)
 print("学習が完了しました。\n")
+'''
 
-#変換に使うサンプル
-flame = 10000
-convertSample = cd[flame]    #ここで別のwavファイルなどを読み込む
-correctSample = hi[flame]
+#高階調化処理
+wav = read_wav_cd("../wav/sample_96000_24bit_000.wav",BEGIN_FLAME,END_FLAME)
+print("変換する振幅データを読み込んでいます。")
+cdAmp = makeAmpArrayCD(wav['amp_l'], BEGIN_FLAME, END_FLAME)
+print("正解振幅データを作成しています。")   #正解を見たい時のオプション
+#originAmp = makeAmpArrayHi(wav['amp_l'], BEGIN_FLAME, END_FLAME)
+
+#変換のみ実行する場合はコメントを外す
+gmm = joblib.load("gmm")    #GMMファイルの読み込み
 
 print("変換を実行します。")
-result_amp = convert([convertSample], gmm, components)
-print("変換が終わりました。\n")
+result = []
+currentP = 0
+for i in range(0,len(cdAmp)) : 
+    convFlame = convert([cdAmp[i]], gmm, components)
+    result.extend(convFlame * 16777216.0)   #24bitに変換
+    
+    #進行度合いの表示
+    nextP = math.floor(i/len(cdAmp)*100)
+    if nextP > currentP : 
+        sys.stdout.write("\r%s" % str(nextP)+"% ")
+        sys.stdout.flush()
+        time.sleep(0.01)
+    currentP = nextP
+sys.stdout.write("\r%s" % str(100)+"% ")
 
-print("変換元 の振幅値: \n",convertSample)
-pl.plot(convertSample)
+b = []
+for j in cdAmp : 
+    b.extend(j)
+    
+c = []
+#for j in originAmp : 
+#    c.extend(j)
+print("\n変換が終わりました。\n")
 
-print("変換結果の振幅値 : \n",result_amp)
-pl.plot(result_amp)
+wavio.writewav24("sample.wav", 96000, result)
 
-print("正解の振幅値 : \n",correctSample)
-pl.plot(correctSample)
-pl.show()
+#波形の表示
+fl1 = 0
+fl2 = len(cdAmp)
+plt.plot(b[fl1:fl2], label = "CD")
+plt.plot(result[fl1:fl2], label = "Result")
+#plt.plot(c[fl1:fl2], label = "Correct")
+plt.legend()
+plt.title("Result")
+plt.xlabel("Time [t]")
+plt.ylabel("Amplitude")
+plt.show()
